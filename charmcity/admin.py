08dfa556d85e585a666eade6b196cf233517225e
Apple <apple@Apples-MacBook-Pro.local>
2024-03-09 18:52:59 +0500

    add models

--- charmcity/admin.py
+++ charmcity/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Cart,Category,Sub_category,Product,Order
 
-# Register your models here.
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+        list_display = ['id', 'name']
+        
+class Sub_categoryAdmin(admin.ModelAdmin):
+    list_display = [ 'id', 'name' , 'category']
+    
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','title', 'description', 'brand' , 'weight' , 'size', 'price' , 'category' ,'sub_category' , 'sale']
+    
+    
+class CartAdmin(admin.ModelAdmin):
+    list_display = [ 'id' , 'product' , 'user' , 'quantity' ,'sub_total']
+    
+    
+    
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [ 'id' , 'first_name' , 'last_name' , 'address' , 'city' , 'phone' , 'postal' , 'is_delivered']
+    
+    
+    
+admin.site.register(Category,CategoryAdmin)   
+admin.site.register(Sub_category,Sub_categoryAdmin)   
+admin.site.register(Product,ProductAdmin)   
+admin.site.register(Cart,CartAdmin)   
+admin.site.register(Order,OrderAdmin)
+
+   
+             
--- charmcity/models.py
+++ charmcity/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -11,4 +12,58 @@ class Category(models.Model):
 
 
 
-   
+class Sub_category(models.Model):
+    name = models.CharField(max_length= 200)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    
+    
+    def __str__(self):
+      return f'{self.name}-- {self.category}'
+  
+  
+  
+  
+  
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    weight = models.DecimalField(max_digits = 10 , decimal_places=2)
+    size = models.DecimalField(max_digits = 10 , decimal_places = 2)
+    price = models.DecimalField(max_digits =10 , decimal_places =2)
+    category = models.ForeignKey(Category , on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(Sub_category, on_delete=models.CASCADE)
+    img = models.ImageField(upload_to=' product-img/' )
+    sale = models.BooleanField(default = False)
+    
+    
+    def __str__(self):
+        return self.name
+    
+    
+    
+    
+    
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete = models.CASCADE)   
+    user = models.ForeignKey(User, on_delete = models.CASCADE)   
+    quantity = models.IntegerField()   
+    sub_total = models.DecimalField(max_digits =10 , decimal_places = 2)   
+    
+    
+       
+    
+    
+class Order(models.Model):
+    first_name = models.CharField(max_length = 200 )
+    last_name = models.CharField(max_length = 200 )
+    address = models.CharField(max_length = 200 )
+    city = models.IntegerField()
+    phone  = models.IntegerField()
+    postal = models.IntegerField()
+    is_delivered  = models.BooleanField(default = False )
+
+        
+    
+    
+      
