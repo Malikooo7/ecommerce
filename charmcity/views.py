0b93f56c734131a161e86793cd8d85d6726809c3
frrukh <mhmdfrrukh13@gmail.com>
2024-03-09 18:55:37 +0500

    making layout

--- charmcity/views.py
+++ charmcity/views.py
@@ -2,11 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 
-
-
-
-
-
 def index(request):
     return render(request , 'index.html')
-    
+
+def signup(request):
+    return render(request, 'signup.html')
