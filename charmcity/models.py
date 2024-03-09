94d2078c0acd99c7a047681d38a522c431681bf3
Apple <apple@Apples-MacBook-Pro.local>
2024-03-09 17:36:20 +0500

    add new class

--- charmcity/models.py
+++ charmcity/models.py
@@ -1,70 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class product(models.Model):
-    name = models.CharField(max_length = 200)
-
-    
-class Cart(models.Model):
-    name = models.CharField(max_length = 200)
-
-class Order(models.Model):
-    name = models.CharField(max_length = 200)
-
-
-class process(models.Model):
-    name = models.CharField(max_length=200)
-
-class NotNew(models.Model):
-    name = models.CharField(max_length=200)
-
-    name = models.CharField(max_length=200)
-
-class NotHope(models.Model):
-    hope_percentage = models.FloatField()
-
-class Goal(models.Model):
-    hope_percentage = models.FloatField()
-
-class GoalSchool(models.Model):
-    hope_percentage = models.FloatField()
-
-class New(models.Model):
-    name = models.CharField(max_length=200)
-
-class Cart(models.Model):
-    name = models.CharField(max_length = 200)
-
-class Order(models.Model):
-    name = models.CharField(max_length = 200)
-
-
-class process(models.Model):
-    name = models.CharField(max_length=200)
-
-class NotNew(models.Model):
-
-    name = models.CharField(max_length=200)
-
-class NotHope(models.Model):
-    hope_percentage = models.FloatField()
-
-class Goal(models.Model):
-    hope_percentage = models.FloatField()
-
-class GoalSchool(models.Model):
-    hope_percentage = models.FloatField()
-
-class New(models.Model):
-    name = models.CharField(max_length=200)
-    
-class Hope(models.Model):
-    name = models.CharField(max_length=200)
+class Category(models.Model):
+    name = models.CharField(max_length = 200) 
     
     
-class New3(models.Model):
-    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
     
-class Hopee(models.Model):
-    name = models.CharField(max_length=200)
 
+
+
+   
