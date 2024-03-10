3f2d013102983274224506af5af48bb3355e7033
frrukh <mhmdfrrukh13@gmail.com>
2024-03-10 06:32:19 +0500

    adding signupform with some custom validations

--- charmcity/views.py
+++ charmcity/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+import json
+
 
 
 
@@ -9,6 +12,10 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request , 'index.html')
-    
+
 def signup(request):
-    return render(request, 'signup.html')
+    users = User.objects.all()
+    a = ' '.join(i.username for i in users)
+    all_users = a.split()
+    json_data = json.dumps(all_users)
+    return render(request, 'signup.html', {'users': json_data})
